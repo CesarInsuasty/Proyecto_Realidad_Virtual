730c7d8ae9c0b6493887623e0961cecb5a8b4ac1
CesarInsuasty <124891124+CesarInsuasty@users.noreply.github.com>
2023-03-23 02:58:37 -0500

    Proyecto

---
+++ Assets/Scripts/ObjectSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSelect : Interactable
+{
+    public Transform playerHand;
+    public Vector3 offset;
+
+    public bool objetoEnMano = false; // Variable para verificar si el objeto está en la mano del jugador
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        gameObject.SetActive(false);
+
+        transform.SetParent(playerHand);
+        transform.localPosition = offset;
+        transform.localRotation = Quaternion.identity;
+        transform.localScale = Vector3.one;
+        transform.Rotate(Vector3.up * 180);
+
+        gameObject.SetActive(true);
+
+        objetoEnMano = true; // Cambia el valor de objetoEnMano a true
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Proyecto_Realidad_Virtual
+ Objetivo: Evita los enemigos, consigue el arma (E) abriendo la puerta, y acaba con todos
